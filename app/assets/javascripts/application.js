68c7c56c2c34139853ef9a168582df92b1ddbe5a
ConservingEarth <conservingearth@gmail.com>
2015-12-14 22:13:06 +0100

    got top nav menu to basic layout and functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,20 @@
 //= require gmaps/google
 //= require groups
 //= require rails.validations
+
+
+$(function () {
+  $('.click-nav > ul').toggleClass('no-js js');
+  $('.click-nav .js ul').hide();
+  $('.click-nav .js').click(function(e) {
+    $('.click-nav .js ul').slideToggle(5);
+    $('.clicker').toggleClass('active');
+    e.stopPropagation();
+  });
+  $(document).click(function() {
+    if ($('.click-nav .js ul').is(':visible')) {
+      $('.click-nav .js ul', this).slideUp();
+      $('.clicker').removeClass('active');
+    }
+  });
+});
