da2d2482aaa6b45936a7ed47e3ad0b407143dfd4
ConservingEarth <conservingearth@gmail.com>
2016-01-26 18:43:57 +0100

    helper
    added some, probaly inrelevent faye code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,11 @@ module ApplicationHelper
   current_page?(link_path) ? "active" : ""
  end
 
+ def broadcast(channel, &block)
+  message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
+  uri = URI.parse("http://localhost:9292/faye")
+  Net::HTTP.post_form(uri, :message => message.to_json)
+end
+
+
 end
