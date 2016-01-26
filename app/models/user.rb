11723e1d2c5a85ad79359fda2b312a67477c821f
ConservingEarth <conservingearth@gmail.com>
2016-01-26 18:47:56 +0100

    models
    updated the user model and created the projects and messages model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,14 @@ class User < ActiveRecord::Base
   has_many :infos, dependent: :destroy
   has_many :posts
   has_many :comments
-  has_many :friendships
+  has_many :projects
+
+  has_many :friendships, :foreign_key => :user_id
   has_many :friends, :through => :friendships
+
   has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
   has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
   has_many :members, :foreign_key => :user_id
   has_many :groups, :through => :members
 
