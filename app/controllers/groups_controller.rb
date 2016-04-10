a3b1f3df1b1c9a813e59f28f9d6a190825a4fc1a
ConservingEarth <conservingearth@gmail.com>
2016-04-11 00:54:40 +0200

    removed doubt before filter

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,6 @@
 class GroupsController < ApplicationController
 	protect_from_forgery with: :exception
 	before_filter :require_user, :only => [:edit, :update, :destroy]
-	before_filter :require_user, :only => [:edit, :update, :destroy]
 	before_filter :authenticate_user!, except: [:index, :show, :map]
 	def search
 		
